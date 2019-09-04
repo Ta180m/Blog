295544f184ed76cec959e04c0fde41ff597e2e9b
Ta180m <ta180m@gmail.com>
2019-09-03 21:52:34 -0500

    Delete script.php

--- script.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if (isset($_POST['token'])) {
-        echo "HTTP 200 OK<br/>Content-type: text/plain<br/>" . $_POST['challenge'];
-    }
-    else {
-        echo "Post not received.";
-    }
-?>
